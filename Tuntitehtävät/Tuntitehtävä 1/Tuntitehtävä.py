3fa4706a17d5574c52d9743d3d6991ff773078cb
RicardsAleksejuks <ricards.aleksejuks@gmail.com>
2026-08-20 10:34:24 +0300

    Tuntitehtävän lisääminen

---
+++ Tuntitehtävät/Tuntitehtävä 1/Tuntitehtävä.py
@@ -0,0 +1,4 @@
+nimi = input("Syötä nimi: ")
+Ase = input("Syötä Ase: ")
+
+print(f"\nPimeys laskeutui valtakunnan ylle, mutta urhea soturi {nimi} astui esiin nostaen ilmaan mahtavan aseensa: {Ase}!")
